1a066d34f767ba37e16d7c5311b2fcc35006c2e8
Joshua <Joshua@Joshuas-MacBook-Air.local>
2018-04-14 15:16:34 -0400

    next4 commit

--- server.js
+++ server.js
@@ -77,48 +77,65 @@ app.get('/pixelTransfer/data?', function(req, res){
 	var url = req.query.url
 	if (url) {
 		if (url.match(/^(http\:|https\:).+[.](gif|png|jpg|jpeg)$/)) {
+			console.log('url is real')
+			console.log(url)
 			getPixels(url, function(err, pixels, reader) {
 				if (err) {
 					console.log(err)
 					return
 				} else {
 					var array = [];
-					if(pixels.shape.length < 4){
-						var width = pixels.shape[0];
-						var height = pixels.shape[1];
-            var frame = [];
-						for(var y = 0;y < height;y++){
-							var row = [];
-							for(var x = 0;x < width;x++){
-								var r = pixels.get(x,y,0);
-								var g = pixels.get(x,y,1);
-								var b = pixels.get(x,y,2);
-								var a = 127 - (pixels.get(x,y,3)*(127/255));
-								var pixel = [r,g,b,a];
-								row.push(pixel);
-							}
-							frame.push(row);
+					var width = pixels.shape[0];
+					var height = pixels.shape[1];
+					var frame = [];
+					for(var y = 0;y < height;y++){
+						var row = [];
+						for(var x = 0;x < width;x++){
+							var r = pixels.get(x,y,0);
+							var g = pixels.get(x,y,1);
+							var b = pixels.get(x,y,2);
+							var a = 127 - (pixels.get(x,y,3)*(127/255));
+							var pixel = [r,g,b,a];
+							row.push(pixel);
 						}
-            array.push(frame)
+						frame.push(row);
+					}
+					array.push(frame)
 					}else{
 						var frames = pixels.shape[0];
 						var width = pixels.shape[1];
 						var height = pixels.shape[2];
+            var state = [];
 						for(var z = 0;z < frames;z++){
 							var frame = [];
+              var frameInfo = reader.frameInfo(z);
+              var dispose = frameInfo.disposal;
 							for(var y = 0;y < height;y++){
+                state[y] = state[y] || [];
 								var row = [];
 								for(var x = 0;x < width;x++){
+                  state[y][x] = state[y][x] || [0,0,0,127];
 									var r = pixels.get(z,x,y,0);
 									var g = pixels.get(z,x,y,1);
 									var b = pixels.get(z,x,y,2);
 									var a = 127 - (pixels.get(z,x,y,3)*(127/255));
-									var pixel = [r,g,b,a];
+									var pixel;
+                  var empty = a === 127;
+									if(empty){
+										pixel = state[y][x].slice();
+									}else{
+										pixel = [r,g,b,a];
+									}
 									row.push(pixel);
+                  if(dispose <= 1 && !empty){ //Dispose 0 (Unspecified) and Dispose 1 (Do Not Dispose) mean save the frame to the state
+                    state[y][x] = pixel.slice();
+                  }else if(dispose === 2){ //Dispose 2 (Restore To Background) means nuke the state
+                    state[y][x] = [0,0,0,127];
+                  } //Dispose 3 (Restore to Previous) means do not save the frame to the state, so no changes to the state are needed
 								}
 								frame.push(row);
 							}
-							frame.push(reader.frameInfo(z).delay*10); //Times 10 because GIF protocols specify delay in terms of 100ths of a second, not milliseconds (which are 1000ths)
+							frame.push(frameInfo.delay*10); //Times 10 because GIF protocols specify delay in terms of 100ths of a second, not milliseconds (which are 1000ths)
 							array.push(frame);
 						}
 					}
